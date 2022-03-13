86f6638c136664a428d451a67d7b10a4fde07603
kangupdate2 <90380152+kangupdate2@users.noreply.github.com>
2022-03-14 02:23:15 +0700

    Add files via upload

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -21,21 +21,20 @@ PAGE_NO = 1
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading...📤"
-    STATUS_DOWNLOADING = "Downloading..."
-    STATUS_CLONING = "Cloning...♻️"
-    STATUS_WAITING = "Queued...📝"
-    STATUS_FAILED = "Failed 🚫. Cleaning Download..."
-    STATUS_PAUSE = "Paused...⭕️"
-    STATUS_ARCHIVING = "Archiving...🔐"
-    STATUS_EXTRACTING = "Extracting...📂"
+    STATUS_UPLOADING = "Mengunggah.. **"
+    STATUS_DOWNLOADING = "Mengunduh.. **"
+    STATUS_CLONING = "Menganddakan...♻️"
+    STATUS_WAITING = "Mengantri...📝"
+    STATUS_FAILED = "Gagal 🚫. membersih Unduh..."
+    STATUS_PAUSE = "dijeda...⭕️"
+    STATUS_ARCHIVING = "Pengarsipan...🔐"
+    STATUS_EXTRACTING = "Mengekstraksi...📂"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['⬜️', '⬜️', '⬜️', '⬜️', '⬜️', '⬜️', '⬜️']
+PROGRESS_INCOMPLETE = ['▇', '▇', '▇', '▇', '▇', '▇', '▇']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-
 
 class setInterval:
     def __init__(self, interval, action):
@@ -65,7 +64,7 @@ def get_readable_file_size(size_in_bytes) -> str:
     try:
         return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
     except IndexError:
-        return 'File too large'
+        return 'File sangat besar'
 
 
 def getDownloadByGid(gid):
@@ -100,7 +99,7 @@ def get_progress_bar_string(status):
     p_str = '🟩' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '🟨' * (PROGRESS_MAX_SIZE - cFull) 
+    p_str += '▇' * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"{p_str}"
     return p_str
 
@@ -119,30 +118,30 @@ def get_readable_message():
         for download in list(download_dict.values()):
             INDEX += 1
             if INDEX > COUNT:
-                msg += f"<b>☞ 🗃️Filename :</b> <code>{download.name()}</code>"
-                msg += f"\n<b>☞ 🚦Status :</b> <i>{download.status()}</i>"
+                msg += f"<b>↳Nama:</b> <code>{download.name()}</code>"
+                msg += f"\n<b>↳🚦Status :</b> <i>{download.status()}</i>"
                 if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                    msg += f"<code>{download.progress()}</code>\n <code>{get_progress_bar_string(download)}</code>"
+                    msg += f"<code>{download.progress()}</code>**\n <code>{get_progress_bar_string(download)}</code>"
                     if download.status() == MirrorStatus.STATUS_CLONING:
-                        msg += f"\n<b>☞ 🚦Cloned:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
+                        msg += f"\n<b>↳🚦Diganda:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> ke <code>{download.size()}</code>"
                     elif download.status() == MirrorStatus.STATUS_UPLOADING:
-                        msg += f"\n<b>☞ 📤Uploaded :</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
+                        msg += f"\n<b>↳📤Diunggah :</b> <code>{get_readable_file_size(download.processed_bytes())}</code> ke <code>{download.size()}</code>"
                     else:
-                        msg += f"\n<b>☞ 📥Downloaded :</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
-                    msg += f"\n<b>☞ ⚡️Speed :</b> <code>{download.speed()}</code>" \
-                            f"\n<b>☞ ⏰ETA :</b> <code>{download.eta()}</code> "
+                        msg += f"\n<b>↳📥Diunduh :</b> <code>{get_readable_file_size(download.processed_bytes())}</code> ke <code>{download.size()}</code>"
+                    msg += f"\n<b>↳⚡️Spd:</b> <code>{download.speed()}</code>" \
+                            f" | <b>⏰Est:</b> <code>{download.eta()}</code> "
                     # if hasattr(download, 'is_torrent'):
                     try:
-                        msg += f"\n<b>☞ Seeders :</b> <code>{download.aria_download().num_seeders}</code>" \
-                            f" | <b>☞ Peers :</b> <code>{download.aria_download().connections}</code>"
+                        msg += f"\n<b>↳Seeders :</b> <code>{download.aria_download().num_seeders}</code>" \
+                            f" | <b>↳Peers :</b> <code>{download.aria_download().connections}</code>"
                     except:
                         pass
                     try:
-                        msg += f"\n<b>☞ Seeders :</b> <code>{download.torrent_info().num_seeds}</code>" \
-                            f" | <b>☞ Leechers :</b> <code>{download.torrent_info().num_leechs}</code>"
+                        msg += f"\n<b>↳Seeders :</b> <code>{download.torrent_info().num_seeds}</code>" \
+                            f" | <b>↳Leechers :</b> <code>{download.torrent_info().num_leechs}</code>"
                     except:
                         pass
-                    msg += f"\n<b>☞ To cancel ❌ :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                    msg += f"\n<b>↳❌Batal:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
                 msg += "\n\n"
                 if STATUS_LIMIT is not None:
                     if INDEX >= COUNT + STATUS_LIMIT:
