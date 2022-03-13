7affb8117ad95a5b93c090c3b53851f108840972
kangupdate2 <90380152+kangupdate2@users.noreply.github.com>
2022-03-13 22:17:06 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -32,7 +32,7 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['✩', '✩', '✩', '✩', '✩', '✩', '✩']
+PROGRESS_INCOMPLETE = ['🟥', '🟥', '🟥', '🟥', '🟥', '🟥', '🟥']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -97,10 +97,10 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '✪' * cFull
+    p_str = '🟩' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '✩' * (PROGRESS_MAX_SIZE - cFull) 
+    p_str += '⬜️' * (PROGRESS_MAX_SIZE - cFull) 
     p_str = f"[{p_str}]"
     return p_str
 
